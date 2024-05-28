8bd61e77281273550c93862daeb552675d141b56
HideTsutsumi <167219646+QtCpiqtq@users.noreply.github.com>
2024-05-28 16:36:18 +0000

    [fix]RSpecの実施

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get 'users/edit'
   devise_for :users
   root to: "homes#top"
-  get "homes/about" => "homes#about", as: "about"
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  get "home/about" => "homes#about", as: "about"
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update, :index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
